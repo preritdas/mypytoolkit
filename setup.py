66ab387f3223f9bd382c33153e63212eb2c07527
Prerit Das <preritdas@gmail.com>
2022-03-28 17:24:48 -0400

    Changed function names in setup.py. No functional differences.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ DESCRIPTION = (
     "Basic tools I feel are missing in the standard Python distribution."
 )
 
-def getReadMe():
+def get_read_me():
     with open("README.md", "r") as f:
         return f.read()
 
@@ -16,7 +16,7 @@ setup(
     author="Prerit Das",
     author_email="<preritdas@gmail.com>",
     description=DESCRIPTION,
-    long_description=getReadMe(),
+    long_description=get_read_me(),
     long_description_content_type="text/markdown",
     packages=find_packages(),
     install_requires = ["DateTime", "matplotlib", "numpy"],
