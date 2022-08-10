b80abd89856216b433b15fb53905d1a23235b3c3
Prerit Das <preritdas@gmail.com>
2022-08-10 01:52:26 -0400

    New web module with link shortening.

---
+++ mypytoolkit/web.py
@@ -0,0 +1,20 @@
+"""
+Web-related actions, including link-shortening, etc.
+"""
+import requests
+
+
+def _bitly(long_link: str, access_token: str) -> str:
+    """Shortens link using the Bitly API. A valid access token is required."""
+    if not isinstance(long_link, str):
+        raise Exception("You must provide a string.")
+
+    headers = {
+        'Authorization': f'Bearer {access_token}',
+        'Content-Type': 'application/json'
+    }
+
+    data = ' {"long_url": "' + long_link + '" } '
+    
+    response = requests.post(url="https://api-ssl.bitly.com/v4/shorten", headers=headers, data=data)
+    return response.json()['link']
