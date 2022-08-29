20dae57b47a82b90da533db3c4d5e60f61625f74
Prerit Das <preritdas@gmail.com>
2022-08-29 19:26:53 -0400

    Write unit tests for dates.

---
+++ tests/test_dates.py
@@ -0,0 +1,21 @@
+from mypytoolkit import dates
+
+
+def test_time_now():
+    dates.time_now()
+
+
+def test_time_decimal():
+    dates.time_decimal()
+
+
+def test_weekly_time_decimal():
+    assert 0 <= dates.weekly_time_decimal() < 7
+
+
+def test_weekday_name():
+    assert dates.weekday_name(1) == "Monday"
+    assert dates.weekday_name(7) == "Sunday"
+    
+    assert dates.weekday_name(1, sunday_start=True) == "Sunday"
+    assert dates.weekday_name(4, sunday_start=True) == "Wednesday"
