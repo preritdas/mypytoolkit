4eb319d577cfb29d587a74a63fa241640758e792
Prerit Das <preritdas@gmail.com>
2022-08-31 22:02:20 -0400

    Bump v number for actions.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 
-VERSION = "1.5.45"
+VERSION = "1.5.46"
 DESCRIPTION = (
     "Basic tools I feel are missing in the standard Python distribution."
 )
