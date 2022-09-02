ddea3d691c5d67d2b48c1587814ca8f9ee3e6231
Prerit Das <preritdas@gmail.com>
2022-09-01 22:43:32 -0400

    Add supported Python versions to read-me.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 
-VERSION = "1.5.47"
+VERSION = "1.5.48"
 DESCRIPTION = (
     "Basic tools I feel are missing in the standard Python distribution."
 )
