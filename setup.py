075458728b9ad8701c7214d17c9ded5830a15558
Prerit Das <preritdas@gmail.com>
2022-10-06 14:03:04 -0400

    Bump version for deployment.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 
-VERSION = "1.5.60"
+VERSION = "1.5.65"
 DESCRIPTION = (
     "Basic tools I feel are missing in the standard Python distribution."
 )
